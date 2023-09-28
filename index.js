5e4a60f384894ece6b7ef2b6358dba1eb592c8d4
Semen <lbdw.smn@gmail.com>
2023-09-28 21:40:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import {registerValidation} from './validations/auth.js'
+import {validationResult} from 'express-validator'
+import UserModel from './models/User.js'
+
+mongoose
+ .connect('mongodb+srv://lbdwsmn:12345@cluster0.1d800tl.mongodb.net/blog?retryWrites=true&w=majority')
+ .then(() => console.log('DB okey'))
+ .catch ((err) => console.log('DB err', err))
+
+const app = express();
+app.use(express.json());
+
+app.post("/auth/register", registerValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array())
+  }
+
+
+  const password = req.body.password
+  const salt = await bcrypt.genSalt(10)
+  const passwordHash = await bcrypt.hash(password, salt)
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl, 
+    passwordHash,
+  })
+
+  const user = await doc.save()
+  
+  res.json(user)
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Все окей!");
+});
