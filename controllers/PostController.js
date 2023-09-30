7ff17ccdf155c2938fa20349dd5d54cf8f357c07
Semen <lbdw.smn@gmail.com>
2023-09-30 20:14:25 +0300

    upload file + delete post + pach post + fix bag

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,7 +2,7 @@ import PostModel from "../models/Post.js";
 
 export const getAll = async (req, res) => {
   try {
-    const posts = await PostModel.find().populate('user').exec();
+    const posts = await PostModel.find().populate("user").exec();
     res.json(posts);
   } catch (error) {
     console.log(err);
@@ -12,11 +12,71 @@ export const getAll = async (req, res) => {
   }
 };
 
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const updatedPost = await PostModel.findByIdAndUpdate(
+      postId,
+      { $inc: { viewsCount: 1 } },
+      { new: true }
+    ).catch((err) => {
+      console.log(err);
+      return res.status(500).json({
+        message: "Не удалось получить статью",
+      });
+    });
+
+    if (!updatedPost) {
+      return res.status(404).json({
+        message: "Статья не найдена",
+      });
+    }
+
+    res.json(updatedPost);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить статью",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const deletedPost = await PostModel.findOneAndDelete({ _id: postId }).catch(
+      (err) => {
+        console.log(err);
+        return res.status(500).json({
+          message: "Не удалось удалить статью",
+        });
+      }
+    );
+
+    if (!deletedPost) {
+      return res.status(404).json({
+        message: "Статья не найдена",
+      });
+    }
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить статью",
+    });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
       title: req.body.title,
-      text: req.body.title,
+      text: req.body.text,
       imageUrl: req.body.imageUrl,
       tags: req.body.tags,
       user: req.userId,
@@ -24,11 +84,50 @@ export const create = async (req, res) => {
 
     const post = await doc.save();
     res.json(post);
-
   } catch (err) {
     console.log(err);
     res.status(500).json({
       message: "Не удалось создать статью",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const updatedPost = await PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId,
+      },
+      { new: true }
+    ).catch((err) => {
+      console.log(err);
+      return res.status(500).json({
+        message: "Не удалось обновить статью",
+      });
+    });
+
+    if (!updatedPost) {
+      return res.status(404).json({
+        message: "Статья не обновилась",
+      });
+    }
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось обновить статью",
     });
   }
 };
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from "multer";
 import { registerValidation } from "./validations/auth.js";
 import { postCreateValidation } from "./validations/post.js";
-import checkAuth from "./utils/checkAuth.js";
-import * as UserController from "./controllers/UserController.js";
-import * as PostController from "./controllers/PostController.js";
+import { checkAuth, handleValidationError } from "./utils/index.js";
+import { UserController, PostController } from './controllers/index.js'
 
 mongoose
   .connect(
@@ -15,16 +15,34 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use("/uploads", express.static('uploads'))
 
-app.post("/auth/login",  UserController.login);
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({storage})
+
+app.post("/auth/login",handleValidationError, UserController.login);
 app.get("/auth/me", checkAuth, UserController.getMe);
-app.post("/auth/register", registerValidation, UserController.register);
+app.post("/auth/register", registerValidation, handleValidationError, UserController.register);
+
+app.post("/upload", checkAuth, upload.single('image'), (req, res) =>{
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  })
+})
 
 app.get("/posts", checkAuth, PostController.getAll)
-// app.get("/posts/:id", PostController.getOne)
+app.get("/posts/:id", PostController.getOne)
 app.post("/posts", checkAuth, postCreateValidation, PostController.create)
-// app.delete("/posts", PostController.remove)
-// app.patch("/posts", PostController.update)
+app.delete("/posts/:id", checkAuth, PostController.remove)
+app.patch("/posts:id", checkAuth, postCreateValidation,PostController.update)
 
 app.listen(4444, (err) => {
   if (err) {
